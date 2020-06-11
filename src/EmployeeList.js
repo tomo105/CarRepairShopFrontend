b084fd33ae6b35d2cf4f84f280f7a8571c6b2180
Franciszek Czana <franciszek.czana@futurum.tech>
2020-06-11 03:25:08 +0200

    refactor popus implementation

--- src/EmployeeList.js
+++ src/EmployeeList.js
@@ -75,11 +75,10 @@ class EmployeeList extends Component {
                             + appointment.nameUser + " ";
                     }
                     if (d >= dateToFormat && d <= yearStamp) {
-                        appointmentAnnual +=
-                            appointment.description + " "
-                            + appointment.data + " "
-                            + appointment.numberCar + " "
-                            + appointment.nameUser + " ";
+                        appointmentAnnual.push(appointment.nameUser);
+                        appointmentAnnual.push(appointment.numberCar);
+                        appointmentAnnual.push(appointment.data);
+                        appointmentAnnual.push(appointment.description);
                     }
                 }
                 return appointmentInformation;
@@ -87,10 +86,36 @@ class EmployeeList extends Component {
             return {appointmentWeekly, appointmentMonthly, appointmentAnnual};
         }
 
+        function AppointmentDetails(props) {
+            let appointment = props.appointment;
+            let count = appointment.length / 4;
+            return <div>
+                <Popup trigger={
+                        <Button color="info" className="button">
+                            {" "} {props.period} Schedule {" "}
+                        </Button>
+                    } modal >
+                    {close => (
+                        <div className="carPopup">
+                            <div className="content">
+                                {" "}
+                                <h1>{props.period} Schedule</h1>
+                                <div className="textFrame">{appointment}</div>
+                            </div>
+                            <div className="actions">
+                                <Button className="button" onClick={() => {
+                                    close(); } }>Close popup window</Button>
+                            </div>
+                        </div>
+                    )}
+                </Popup>
+            </div>
+        }
+
         const allEmployees = employees.map(employee => {
             let appointmentWeekly = "";
             let appointmentMonthly = "";
-            let appointmentAnnual = "";
+            let appointmentAnnual = [];
             const __ret = appointmentsMap(employee, appointmentWeekly, appointmentMonthly, appointmentAnnual);
             appointmentWeekly = __ret.appointmentWeekly;
             appointmentMonthly = __ret.appointmentMonthly;
@@ -103,104 +128,17 @@ class EmployeeList extends Component {
                     <td>{employee.experienceInCompany}</td>
                     <td>{employee.setRole}</td>
                     <td>
-                        <Popup
-                            trigger={
-                                <Button color="info" className="button">
-                                    {" "}
-                                    Weekly Schedule{" "}
-                                </Button>
-                            }
-                            modal
-                        >
-                            {close => (
-                                <div className="carPopup">
-                                    <div className="content">
-                                        {" "}
-                                        <h1>Weekly Schedule</h1>
-                                        <div className="textFrame">{appointmentWeekly}</div>
-                                    </div>
-                                    <div className="actions">
-                                        <Button className="button" onClick={() => {
-                                                console.log("popup windows closed ");
-                                                close();
-                                            }
-                                        }>Close popup window
-                                        </Button>
-                                    </div>
-                                </div>
-                            )}
-                        </Popup>
+                        <AppointmentDetails appointment={appointmentWeekly} period="Weekly"/>
                     </td>
                     <td>
-                        <Popup
-                            trigger={
-                                <Button color="info" className="button">
-                                    {" "}
-                                    Monthly Schedule{" "}
-                                </Button>
-                            }
-                            modal
-                        >
-                            {close => (
-                                <div className="carPopup">
-                                    <div className="content">
-                                        {" "}
-                                        <h1>Monthly Schedule</h1>
-                                        <div className="textFrame">{appointmentMonthly}</div>
-                                    </div>
-                                    <div className="actions">
-                                        <Button className="button" onClick={() => {
-                                                console.log("popup windows closed ");
-                                                close();
-                                            }
-                                        }> Close popup window
-                                        </Button>
-                                    </div>
-                                </div>
-                            )}
-                        </Popup>
+                        <AppointmentDetails appointment={appointmentMonthly} period="Monthly"/>
                     </td>
                     <td>
-                        <Popup
-                            trigger={
-                                <Button color="info" className="button">
-                                    {" "}
-                                    Annual Schedule{" "}
-                                </Button>
-                            }
-                            modal
-                        >
-                            {close => (
-                                <div className="carPopup">
-                                    <div className="content">
-                                        {" "}
-                                        <h1>Annual Schedule</h1>
-                                        <div className="textFrame">{appointmentAnnual}</div>
-                                    </div>
-                                    <div className="actions">
-                                        <Button
-                                            className="button"
-                                            onClick={() => {
-                                                console.log("popup windows closed ");
-                                                close();
-                                            }}
-                                        >
-                                            Close popup window
-                                        </Button>
-                                    </div>
-                                </div>
-                            )}
-                        </Popup>
+                        <AppointmentDetails appointment={appointmentAnnual} period="Annual"/>
                     </td>
                     <td>
-                        <Button
-                            size="md"
-                            color="primary"
-                            tag={Link}
-                            to={"/employees/" + employee.id}
-                        >
-                            Edit
-                        </Button>
+                        <Button size="md" color="primary" tag={Link}
+                                to={"/employees/" + employee.id}>Edit</Button>
                     </td>
                 </tr>
             );
@@ -209,7 +147,7 @@ class EmployeeList extends Component {
         const employeeList = employees.map(employee => {
             let appointmentWeekly = "";
             let appointmentMonthly = "";
-            let appointmentAnnual = "";
+            let appointmentAnnual = [];
             if (this.state.whichOne !== null && employee.setRole === this.state.whichOne) {
                 appointmentsMap(employee, appointmentWeekly, appointmentMonthly, appointmentAnnual);
                 return (
@@ -220,164 +158,23 @@ class EmployeeList extends Component {
                         <td>{employee.experienceInCompany}</td>
                         <td>{employee.setRole}</td>
                         <td>
-                            <Popup
-                                trigger={
-                                    <Button color="info" className="button">
-                                        {" "}
-                                        Weekly Schedule{" "}
-                                    </Button>
-                                }
-                                modal
-                            >
-                                {close => (
-                                    <div className="carPopup">
-                                        <div className="header"> Weekly Schedule </div>
-                                        <div className="content">
-                                            {" "}
-                                            <h1>Appointment information</h1>
-                                            <div className="textFrame">{appointmentWeekly}</div>
-                                        </div>
-                                        <div className="actions">
-                                            <Button
-                                                className="button"
-                                                onClick={() => {
-                                                    console.log("popup windows closed ");
-                                                    close();
-                                                }}
-                                            >
-                                                Close popup window
-                                            </Button>
-                                        </div>
-                                    </div>
-                                )}
-                            </Popup>
+                            <AppointmentDetails appointment={appointmentWeekly} period="Weekly"/>
                         </td>
                         <td>
-                            <Popup
-                                trigger={
-                                    <Button color="info" className="button">
-                                        {" "}
-                                        Monthly Schedule{" "}
-                                    </Button>
-                                }
-                                modal
-                            >
-                                {close => (
-                                    <div className="carPopup">
-                                        <div className="header"> Monthly Schedule </div>
-                                        <div className="content">
-                                            {" "}
-                                            <h1>Appointment information</h1>
-                                            <div className="textFrame">{appointmentMonthly}</div>
-                                        </div>
-                                        <div className="actions">
-                                            <Button
-                                                className="button"
-                                                onClick={() => {
-                                                    console.log("popup windows closed ");
-                                                    close();
-                                                }}
-                                            >
-                                                Close popup window
-                                            </Button>
-                                        </div>
-                                    </div>
-                                )}
-                            </Popup>
+                            <AppointmentDetails appointment={appointmentMonthly} period="Monthly"/>
                         </td>
                         <td>
-                            <Popup
-                                trigger={
-                                    <Button color="info" className="button">
-                                        {" "}
-                                        Annual Schedule{" "}
-                                    </Button>
-                                }
-                                modal
-                            >
-                                {close => (
-                                    <div className="carPopup">
-                                        <div className="header"> Annual Schedule </div>
-                                        <div className="content">
-                                            {" "}
-                                            <h1>Appointment information</h1>
-                                            <div className="textFrame">{appointmentAnnual}</div>
-                                        </div>
-                                        <div className="actions">
-                                            <Button
-                                                className="button"
-                                                onClick={() => {
-                                                    console.log("popup windows closed ");
-                                                    close();
-                                                }}
-                                            >
-                                                Close popup window
-                                            </Button>
-                                        </div>
-                                    </div>
-                                )}
-                            </Popup>
+                            <AppointmentDetails appointment={appointmentAnnual} period="Annual"/>
                         </td>
                         <td>
-                            {" "}
-                            <Popup
-                                trigger={
-                                    <Button color="info" className="button">
-                                        {" "}
-                                        Show Information{" "}
-                                    </Button>
-                                }
-                                modal
-                            >
-                                {close => (
-                                    <div className="carPopup">
-                                        <div className="header"> Employee Information </div>
-                                        <div className="content">
-                                            {" "}
-                                            <h1>Name</h1>
-                                            <div className="textFrame">{employee.name}</div>
-                                            <h1>Surname</h1>
-                                            <div className="textFrame">{employee.surname}</div>
-                                            <h1>Experience</h1>
-                                            <div className="textFrame">{employee.experience}</div>
-                                            <h1>Experience in company</h1>
-                                            <div className="textFrame">
-                                                {employee.experienceInCompany}
-                                            </div>
-                                            <h1>Role</h1>
-                                            <div className="textFrame">{employee.role}</div>
-                                        </div>
-                                        <div className="actions">
-                                            <Button
-                                                className="button"
-                                                onClick={() => {
-                                                    console.log("popup windows closed ");
-                                                    close();
-                                                }}
-                                            >
-                                                Close popup window
-                                            </Button>
-                                        </div>
-                                    </div>
-                                )}
-                            </Popup>
-                        </td>
-                        <td>
-                            <Button
-                                size="md"
-                                color="primary"
-                                tag={Link}
-                                to={"/employees/" + employee.id}
-                            >
-                                Edit
-                            </Button>
+                            <Button size="md" color="primary" tag={Link}
+                                    to={"/employees/" + employee.id}>Edit</Button>
                         </td>
                     </tr>
                 );
             }
             return <div/>
         });
-
 
         const isRole = this.state.whichOne;
         let res;
