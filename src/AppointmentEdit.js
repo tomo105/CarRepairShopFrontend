84568e8be1a755f468de30278a6c0c191933bc21
Franciszek Czana <franciszek.czana@futurum.tech>
2020-06-13 16:23:19 +0200

    set custom roles on appointment select employee

--- src/AppointmentEdit.js
+++ src/AppointmentEdit.js
@@ -91,6 +91,12 @@ class AppointmentEdit extends Component {
     }
 
     let usersList = [];
+    for (let i = 0; i < employees.length; i++) {
+      if (employees[i].setRole !== "Mechanic" &&
+          employees[i].setRole !== "Manager") {
+        employees.splice(i--,1);
+      }
+    }
     employees.map(employee => {
       usersList.push(employee.surname);
     });
